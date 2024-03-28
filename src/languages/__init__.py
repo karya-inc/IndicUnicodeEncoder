b804375958fedec600916428ad7913715a83a666
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2024-03-29 01:56:36 +0530

    feat: add encoder for limbu

--- src/languages/__init__.py
+++ src/languages/__init__.py
@@ -1,10 +1,13 @@
 from src.languages.lepcha import LepchaEncoder
+from src.languages.limbu import LimbuEncoder
 
 
 def get_ecoder(lang: str):
     match lang:
         case "lepcha":
             return LepchaEncoder
+        case "limbu":
+            return LimbuEncoder
         case _:
             raise AttributeError(f"Unsupported Language: {lang}")
 
